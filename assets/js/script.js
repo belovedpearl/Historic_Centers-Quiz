3c2b9b42b0f172a2f8a44510fcf488eb12a20ab7
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-04-10 13:51:12 +0000

    Add questions to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,120 +1,107 @@
-// questions = [
-//     {
-//         question: "assets/images/quizImages/acropolis-athens.webp",
-//         option1: "Acapolis Athens",
-//         option2: "Athens Lavent",
-//         option3: "Acapolis Lavent",
-//         option4: "Lavent Acapolis",
-//         answer: "Acapolis Athens"
-//     },
-//     {
-//         question: "assets/images/quizImages/ankor-wat-temple.webp",
-//         option1: "Angkor Siem",
-//         option2: "Athens Greece",
-//         option3: "Ankor Wat Cmbodia",
-//         option4: "Bayon Temple",
-//         answer: "Ankor Wat Cmbodia"
-//     },
-//     {
-//         question: "assets/images/quizImages/easter-island.webp",
-//         option1: "Puna Pau Island",
-//         option2: "Easter Island Chile",
-//         option3: "Museum Santiago",
-//         option4: "Hanga Roa",
-//         answer: "Easter Island Chile"
-//     },
-//     {
-//         question: "assets/images/quizImages/borobudur-indonesia.webp",
-//         option1: "Boudur Indonesia",
-//         option2: "Badrawati China",
-//         option3: "Borobudur Indonesia",
-//         option4: "Borobudur Malaysia",
-//         answer: "Borobudur Indonesia"
-//     },
-//     {
-//         question: "assets/images/quizImages/efes-turkey.webp",
-//         option1: "Arzawan Home",
-//         option2: "Efes Turkey",
-//         option3: "Artemis Turkey",
-//         option4: "Ephesos Celsus",
-//         answer: "Efes Turkey"
-//     },
-//     {
-//         question: "assets/images/quizImages/colosseum-rome.webp",
-//         option1: "Colosseum Rome Italy",
-//         option2: "Greater Hall India",
-//         option3: "Colosseum Hall China",
-//         option4: "Doloseum Italy",
-//         answer: "Colosseum Rome Italy"
-//     },
-//     {
-//         question: "assets/images/quizImages/effiel-tower.webp",
-//         option1: "Leaning Tower of Pisa",
-//         option2: "Kjiv TV Tower Ukraine",
-//         option3: "Effiel Tower France",
-//         option4: "Avala Tower Serbia",
-//         answer: "Effiel Tower France"
-//     },
-//     {
-//         question: "assets/images/quizImages/forbidden-city-beijing.webp",
-//         option1: "Forbidden City Beijing",
-//         option2: "Tranducheng Shangai",
-//         option3: "Nanjing Road China",
-//         option4: "Lingerin Garden Budha",
-//         answer: "Forbidden City Beijing"
-//     },
-//     {
-//         question: "assets/images/quizImages/greatsphinx-giza.webp",
-//         option1: "Great Sphere of Giza Isreal",
-//         option2: "Pyramid of Khafre",
-//         option3: "Pyramid of Djoser",
-//         option4: "Great Sphinx of Giza Egypt",
-//         answer: "Great Sphinx of Giza Egypt"
-//     },
-//     {
-//         question: "assets/images/quizImages/great-wallof-china.webp",
-//         option1: "Great Wall of China",
-//         option2: "Hadrian's Wall",
-//         option3: "Berlin Wall",
-//         option4: "Western wall",
-//         answer: "Great Wall of China"
-//     },
-//     {
-//         question: "assets/images/quizImages/hagia-sophia.webp",
-//         option1: "Hagia Irene Istanbul",
-//         option2: "Sultan Wall Istanbul",
-//         option3: "Hagia Sophia Istanbul",
-//         option4: "None of the Above",
-//         answer: "Hagia Sophia Istanbul"
-//     },
-//     {
-//         question: "assets/images/quizImages/london-tower.webp",
-//         option1: "Shangai Tower",
-//         option2: "London Tower",
-//         option3: "Royal Clock Tower Mecca",
-//         option4: "Trade Center New York",
-//         answer: "London Tower"
-//     },
-//     {
-//         question: "assets/images/quizImages/machu-picchu.webp",
-//         option1: "Machu Picchu Peru",
-//         option2: "Ankor Wat Cambodia",
-//         option3: "Bayon Temple-Paris",
-//         option4: "Cerro Machu Picchu",
-//         answer: "Machu Picchu Peru"
-//     },
-//     {
-//         question: "assets/images/quizImages/machu-picchu.webp",
-//         option1: "Machu Picchu Peru",
-//         option2: "Ankor Wat Cambodia",
-//         option3: "Bayon Temple-Paris",
-//         option4: "Cerro Machu Picchu",
-//         answer: "Machu Picchu Peru"
-//     },
+questions = [
+    {
+        question: "assets/images/quizImages/acropolis-athens.webp",
+        options: ["Acapolis Athens","Athens Lavent","Acapolis Lavent", "Lavent Acapolis"],
+        answer: "Acapolis Athens"
+    },
+    {
+        question: "assets/images/quizImages/ankor-wat-temple.webp",
+        options: ["Angkor Siem","Athens Greece","Ankor Wat Cmbodia","Bayon Temple"],
+        answer: "Ankor Wat Cmbodia"
+    },
+    {
+        question: "assets/images/quizImages/easter-island.webp",
+        options: ["Puna Pau Island","Easter Island Chile","Museum Santiago","Hanga Roa"],
+        answer: "Easter Island Chile"
+    },
+    {
+        question: "assets/images/quizImages/borobudur-indonesia.webp",
+        options: ["Boudur Indonesia","Badrawati China","Borobudur Indonesia","Borobudur Malaysia"],
+        answer: "Borobudur Indonesia"
+    },
+    {
+        question: "assets/images/quizImages/efes-turkey.webp",
+        options: ["Arzawan Home","Efes Turkey","Artemis Turkey","Ephesos Celsus"],
+        answer: "Efes Turkey"
+    },
+    {
+        question: "assets/images/quizImages/colosseum-rome.webp",
+        options: ["Colosseum Rome Italy","Greater Hall India","Colosseum Hall China","Doloseum Italy"],
+        answer: "Colosseum Rome Italy"
+    },
+    {
+        question: "assets/images/quizImages/effiel-tower.webp",
+        options: ["Leaning Tower of Pisa","Kjiv TV Tower Ukraine","Effiel Tower France","Avala Tower Serbia"],
+        answer: "Effiel Tower France"
+    },
+    {
+        question: "assets/images/quizImages/forbidden-city-beijing.webp",
+        options: ["Forbidden City Beijing","Tranducheng Shangai",option3: "Nanjing Road China","Lingerin Garden Budha"],
+        answer: "Forbidden City Beijing"
+    },
+    {
+        question: "assets/images/quizImages/greatsphinx-giza.webp",
+        options: ["Great Sphere of Giza Isreal","Pyramid of Khafre","Pyramid of Djoser","Great Sphinx of Giza Egypt"],
+        answer: "Great Sphinx of Giza Egypt"
+    },
+    {
+        question: "assets/images/quizImages/great-wallof-china.webp",
+        options: ["Great Wall of China","Hadrian's Wall","Berlin Wall","Western wall"],
+        answer: "Great Wall of China"
+    },
+    {
+        question: "assets/images/quizImages/hagia-sophia.webp",
+        options: ["Hagia Irene Istanbul","Sultan Wall Istanbul","Hagia Sophia Istanbul","None of the Above"],
+        answer: "Hagia Sophia Istanbul"
+    },
+    {
+        question: "assets/images/quizImages/london-tower.webp",
+        options: ["Shangai Tower","London Tower","Royal Clock Tower Mecca","Trade Center New York"],
+        answer:  "London Tower"
+    },
+    {
+        question: "assets/images/quizImages/machu-picchu.webp",
+        options: ["Machu Picchu Peru","Ankor Wat Cambodia","Bayon Temple-Paris","Cerro Machu Picchu"],
+        answer: "Machu Picchu Peru"
+    },
+    {
+        question: "assets/images/quizImages/machu-picchu.webp",
+        options: ["Machu Picchu Peru","Ankor Wat Cambodia","Bayon Temple-Paris","Cerro Machu Picchu"],
+        answer: "Machu Picchu Peru"
+    },
 
-
+    {
+        question: "assets/images/quizImage/mount-rushmore.webp",
+        options: ["Mount Argus Dublin","Mount Ararat Turkey","Mount Rushmore United States","Mount Arbet Isreal"],
+        answer: "Mount Rushmore United States"
+    },
+    {
+        question: "assets/images/quizImage/petra.webp",
+        options: ["Ankor ","Acropolis Athens","Petra Jordan","Taj Mahal"],
+        answer: "Petra Jordan"
+    },
+    {
+        question: "assets/images/quizImage/pyramid-of-kukulkan.webp",
+        options: ["Pyramid of Tiza Mexico","Pyramid of Kukulkan","Pyramid of Giza","Pyramid of Cholula Mexico"],
+        answer: "Pyramid of Kukulkan"
+    },
+    {
+        question: "assets/images/quizImage/stonehenge-england.webp",
+        options: ["Stone Henge England","Avebury England","Woodhenge England","Stone Henge Nebraska"],
+        answer: "Stone Henge England"
+    },
+    {
+        question: "assets/images/quizImage/taj-mahal.webp",
+        options: ["Petra Jordan","Shah Jahan Paris","Taj Mahal India","Ankor Wat"],
+        answer: "Taj Mahal India"
+    },
+    {
+        question: "assets/images/quizImage/westminster-palace.webp",
+        options: ["Kensington Palace London","Central White Hall Palace","Westminster Palace London","Buckingham Palace"],
+        answer: "Westminster Palace London"
+    },
     
-//]
+]
 
 function instruct(){
     document.getElementById("firstPage").classList.add("hide"); 
@@ -153,6 +140,13 @@ function startGame(){
     let welcomePage = document.getElementById("welcome");
     welcomePage.classList.add("hide");
     runGame()
+}
+
+function runGame(question){
+    const questionIndex = Math.floor(Math.random() * 5);
+    let availableQuestion = question
+    console.log(availableQuestion)
+    let currentQuestion = availableQuestion[questionIndex];
 }
 
 
